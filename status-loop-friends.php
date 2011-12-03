76c84ca3f2497a02414c0e19e8c0f6e376c2f1f7
hnla <hugoashmore@tiscali.co.uk>
2011-12-03 12:33:46 +0000

    Comment out debug function

--- status-loop-friends.php
+++ status-loop-friends.php
@@ -3,7 +3,7 @@ global $members_template, $bp;
 		$user = $bp->loggedin_user->id;
 				if( bp_has_members('user_id=' . $user . '') && $user !== 0 ) : ?>
 					
-					<ul id="friends-list" class="looplist"><li style="color: #fff;"><?php echo $user ?></li>
+					<ul id="friends-list" class="looplist"><?php // echo '<li style="color: #fff;">' . $user . '</li>' ?>
 		<?php	while ( bp_members() ) : bp_the_member(); ?>
 						<li>
 							<div class="item-avatar"><a href="<?php bp_member_permalink() ?>"><?php  bp_member_avatar('type=full&width=30&height=30') ?></a></div>
