4be8e9c1d5341dd69ab2f6e703f1ac016247d3c6
Tammie Lister <karmatosed@gmail.com>
2011-12-14 12:10:11 +0000

    Better responsive
    Added in mobile first fix for profiles, Fixed lot of responsive issues.
     Fixed comment.php output for sub comments on activity.

--- activity/comment.php
+++ activity/comment.php
@@ -20,16 +20,6 @@
 			<?php bp_activity_avatar( 'type=thumb&user_id=' . bp_get_activity_comment_user_id() ); ?>
 		</a>
 	</div>
-
-	<div class="acomment-meta">
-		<?php
-		/* translators: 1: user profile link, 2: user name, 3: activity permalink, 4: activity timestamp */
-		printf( __( '<a href="%1$s">%2$s</a> replied <a href="%3$s" class="activity-time-since"><span class="time-since">%4$s</span></a>', 'status' ), bp_get_activity_comment_user_link(), bp_get_activity_comment_name(), bp_get_activity_thread_permalink(), bp_get_activity_comment_date_recorded() );
-		?>
-	</div>
-
-	<div class="acomment-content"><?php bp_activity_comment_content(); ?></div>
-
 	<div class="acomment-options">
 
 		<?php if ( is_user_logged_in() && bp_activity_can_comment_reply( bp_activity_current_comment() ) ) : ?>
@@ -47,6 +37,15 @@
 		<?php do_action( 'bp_activity_comment_options' ); ?>
 
 	</div>
+
+	<div class="acomment-meta">
+		<?php
+		/* translators: 1: user profile link, 2: user name, 3: activity permalink, 4: activity timestamp */
+		printf( __( '<a href="%1$s">%2$s</a> replied <a href="%3$s" class="activity-time-since"><span class="time-since">%4$s</span></a>', 'status' ), bp_get_activity_comment_user_link(), bp_get_activity_comment_name(), bp_get_activity_thread_permalink(), bp_get_activity_comment_date_recorded() );
+		?>
+	</div>
+
+	<div class="acomment-content"><?php bp_activity_comment_content(); ?></div>
 
 	<?php bp_activity_recurse_comments( bp_activity_current_comment() ); ?>
 </li>
