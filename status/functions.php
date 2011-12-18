d5ab6e817f3ed2c89c2d60a194038bc9d266ce07
Tammie Lister <karmatosed@gmail.com>
2011-12-18 12:20:32 +0000

    commenting out js to show and hide

--- status/functions.php
+++ status/functions.php
@@ -28,7 +28,7 @@ function status_load_scripts() {
 	if ( !is_admin() ) { 
 		wp_enqueue_script("jquery");
 		wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/_inc/scripts/modernizr.js', array("jquery"), '2.0');
-		wp_enqueue_script('status-scripts', get_stylesheet_directory_uri() . '/_inc/scripts/status-scripts.js', array("jquery"), status_get_file_last_mod('status-scripts.js'));
+		//wp_enqueue_script('status-scripts', get_stylesheet_directory_uri() . '/_inc/scripts/status-scripts.js', array("jquery"), status_get_file_last_mod('status-scripts.js'));
 		if ( is_singular() && get_option( 'thread_comments' ) && comments_open() )
 			wp_enqueue_script( 'comment-reply' );
 	}
