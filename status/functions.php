f1822e3851fadf2b07c777280fa6c5fcaf9bc177
hnla <hugoashmore@tiscali.co.uk>
2011-12-18 19:29:25 +0000

    Add global.js for convenience - move and delete. Uncomment enqueue scripts

--- status/functions.php
+++ status/functions.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'status_load_scripts' ) ) :
 function status_load_scripts() {
 	if ( !is_admin() ) { 
 		wp_enqueue_script("jquery");
-		wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/_inc/scripts/modernizr.js', array("jquery"), '2.0');
+		//wp_enqueue_script('modernizr', get_stylesheet_directory_uri() . '/_inc/scripts/modernizr.js', array("jquery"), '2.0');
 		//wp_enqueue_script('status-scripts', get_stylesheet_directory_uri() . '/_inc/scripts/status-scripts.js', array("jquery"), status_get_file_last_mod('status-scripts.js'));
 		if ( is_singular() && get_option( 'thread_comments' ) && comments_open() )
 			wp_enqueue_script( 'comment-reply' );
