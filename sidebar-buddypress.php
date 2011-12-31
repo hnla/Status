cc89b74a7798266893d1ee1385c3d80d81de1f15
Tammie Lister <karmatosed@gmail.com>
2011-12-31 15:51:30 +0000

    Sidebar to an article not section for friends loop

--- sidebar-buddypress.php
+++ sidebar-buddypress.php
@@ -12,18 +12,16 @@
 <aside id="sidebar" class="secondary widget-area" role="complementary">
 	<?php do_action( 'bp_inside_before_sidebar' ) ?>
 	<?php do_action( 'bp_before_sidebar_me' ) ?>
-	<section id="friends-loop">
-		<div class="widget">
-			<h3 class="widgettitle"><?php _e( 'Your Friends', 'status' ) ?></h3>
-			<?php status_showfriends();?>
-		</div>
-	</section>
 	<section id="sidebar-me">
+		<article id="friends-loop">
+			<div class="widget">
+				<h3 class="widgettitle"><?php _e( 'Your Friends', 'status' ) ?></h3>
+				<?php status_showfriends();?>
+			</div>
+		</article>
 		<?php do_action( 'bp_sidebar_me' ) ?>
 	</section>
 	<?php do_action( 'bp_after_sidebar_me' ) ?>
-	<?php do_action( 'bp_inside_before_sidebar' ) ?>
-
 	<?php /* Show forum tags on the forums directory */
 	if ( bp_is_active( 'forums' ) && bp_is_forums_component() && bp_is_directory() ) : ?>
 		<section id="forum-directory-tags" class="widget tags">
