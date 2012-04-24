3916f73644ce889323295fdc97efaa3db077859c
karmatosed <karmatosed@gmail.com>
2012-04-24 20:17:40 +0100

    Remove need to be logged in to see activity for now
    Note : WIll make this a template next few days.

--- index.php
+++ index.php
@@ -8,6 +8,5 @@
 ?>
 
 <?php get_header(); ?>
-			
-						<?php locate_template( array( 'activity/index.php' ), true ); ?>
+			<?php locate_template( array( 'activity/index.php' ), true ); ?>
 <?php get_footer( ); ?>
